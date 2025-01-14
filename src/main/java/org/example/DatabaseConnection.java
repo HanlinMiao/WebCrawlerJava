6d8ec224661a22ba7bd74c14d01c2598095119f3
HanlinMiao <hanlin980505@gmail.com>
2025-01-13 23:13:54 -0500

    implemented DatabaseConnection class with mySQL connector and Jsoup

---
+++ src/main/java/org/example/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // Singleton Class --> Objected Oriented Programming Pattern
+    // Class with only single instance
+    // Because when we are setting up the connection from Java to MySQL
+    // It is a costly operation and having multiple connections are not necessary
+    static Connection connection = null;
+    public Connection getConnection() {
+        // if pre-existing connection exists
+        // return the existing connection
+        if (connection != null) {
+            return connection;
+        }
+        String user ="root";
+        String password = "Miao%980505";
+        String db_name = "searchengineapp";
+        return getConnection(user, password, db_name);
+    }
+
+    private Connection getConnection(String user, String password, String db_name) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/" + db_name + "?user=" + user + "?password" + password);
+        }
+        catch (SQLException | ClassNotFoundException Exception) {
+            Exception.printStackTrace();
+        }
+        return connection;
+    }
+}
